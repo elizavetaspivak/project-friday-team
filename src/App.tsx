c6c75bf1b9cb7196abe605cbf715a5448841078f
elizavetaspivak <elizazbethspivak99@gmail.com>
2021-07-22 10:10:30 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import {Login} from './Login/Login';
 import {Register} from './Register/Register';
@@ -14,22 +14,22 @@ function App() {
     return (
         <div className="App">
             <Nav/>
-            <Switch>
-                <Route path="/login"
-                       render={() => <Login/>}/>
-                <Route path="/register"
-                       render={() => <Register/>}/>
-                <Route path="/profile"
-                       render={() => <Profile/>}/>
-                <Route exact path="/recovery"
-                       render={() => <PasswordRecovery/>}/>
-                <Route exact path="/newpassword"
-                       render={() => <TypeNewPassword/>}/>
-                <Route exact path="/testpage"
-                       render={() => <TestPage/>}/>
-                <Route exact path="*"
-                       render={() => <Error/>}/>
-            </Switch>
+                <Switch>
+                    <Route path="/login"
+                           component={Login}/>
+                    <Route path="/register"
+                           render={() => <Register/>}/>
+                    <Route path="/profile"
+                           render={() => <Profile/>}/>
+                    <Route exact path="/recovery"
+                           render={() => <PasswordRecovery/>}/>
+                    <Route exact path="/newpassword"
+                           render={() => <TypeNewPassword/>}/>
+                    <Route exact path="/testpage"
+                           render={() => <TestPage/>}/>
+                    <Route exact path="*"
+                           render={() => <Error/>}/>
+                </Switch>
         </div>
     );
 }
