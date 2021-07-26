66c556bbda973972cf2ac767ee90351b93acb146
tanya0311 <tanya03.11@mail.ru>
2021-07-26 16:06:54 +0300

    login and logout

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,9 +1,42 @@
-import s from './Profile.module.css'
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { ResponseLoginType } from "../Login/loginApi";
+import { logoutTC } from "../state/login-reducer";
+// import { getUserDataTC } from '../state/login-reducer';
+import { AppRootStateType } from "../state/store";
+import s from "./Profile.module.css";
 
 export function Profile() {
-    return (
-        <div className={s.profile}>
-            Profile
-        </div>
-    );
-};
+  const isLoginIn = useSelector<AppRootStateType, boolean>(
+    (state) => state.login.isLoggedIn
+  );
+
+//   const userData = useSelector<AppRootStateType, ResponseLoginType | {}  >(state=> state.login.user)
+
+  const dispatch = useDispatch();
+  
+//   useEffect(()=> {
+//   axios.post("https://neko-back.herokuapp.com/2.0/auth/me", {}, {withCredentials: true}).then(res=>{
+    //   debugger
+    //   dispatch(getUserDataTC(res.data))
+//   })
+//   },[])
+
+  const logoutHandler = () => {
+    dispatch(logoutTC());
+  };
+  if (!isLoginIn) {
+    return <Redirect to={"/login"} />;
+  }
+  return (
+    <div className={s.profile}>
+      Profile
+      <div>
+          {/* {userData} */}
+        <button onClick={logoutHandler}>Sing out</button>
+      </div>
+    </div>
+  );
+}
--- src/state/login-reducer.ts
+++ src/state/login-reducer.ts
@@ -1,8 +1,65 @@
-export const loginReducer = (state = '', action:any) => {
-    switch (action.type) {
-        case '':
-            return state
-        default:
-            return state
-    }
-}
+import { Dispatch } from "redux";
+import { loginApi, LoginType, ResponseLoginType } from "../Login/loginApi";
+
+const initialState = {
+  isLoggedIn: false as boolean,
+  user: {} as ResponseLoginType | {},
+};
+
+export const loginReducer = (
+  state: InitialStateType = initialState,
+  action: ActionsType
+): InitialStateType => {
+  switch (action.type) {
+    case "LOGIN/SET-IS-LOGGED-IN":
+      return { ...state, isLoggedIn: action.value };
+    case "LOGIN/SET_USER_DATA":
+      // debugger
+      return { ...state, user: action.userData };
+    default:
+      return state;
+  }
+};
+
+// actions
+export const setIsLoggedInAC = (value: boolean) =>
+  ({ type: "LOGIN/SET-IS-LOGGED-IN", value } as const);
+
+export const setUserDataAC = (userData: ResponseLoginType) =>
+  ({
+    type: "LOGIN/SET_USER_DATA",
+    userData,
+  } as const);
+
+// thunks
+export const loginTC =
+  (data: LoginType) => (dispatch: Dispatch<ActionsType>) => {
+    loginApi
+      .login(data)
+      .then((res) => {
+        dispatch(setIsLoggedInAC(true));
+      })
+      .catch();
+  };
+export const logoutTC = () => (dispatch: Dispatch<ActionsType>) => {
+  loginApi
+    .logout()
+    .then((res) => {
+      dispatch(setIsLoggedInAC(false));
+    })
+    .catch();
+};
+
+// export const getUserDataTC =
+//   (data: ResponseLoginType) => (dispatch: Dispatch<ActionsType>) => {
+//     debugger
+//     dispatch(setUserDataAC(data));
+//     dispatch(setIsLoggedInAC(true));
+//   };
+
+// types
+type InitialStateType = typeof initialState;
+
+type ActionsType =
+  | ReturnType<typeof setIsLoggedInAC>
+  | ReturnType<typeof setUserDataAC>;
