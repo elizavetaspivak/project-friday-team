dc5b183049128cef4d0026a328a185031b67560f
elizavetaspivak <elizazbethspivak99@gmail.com>
2021-07-29 23:12:42 +0300

    refactor login reducer and login page

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,43 +1,34 @@
-import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useParams } from "react-router-dom";
-import { ResponseLoginType } from "../dal/api";
-import { logoutTC, setIsLoggedInAC, setUserDataAC } from "../state/login-reducer";
-import { getMeTC } from "../state/profile-reducer";
-import { AppRootStateType } from "../state/store";
-import s from "./Profile.module.css";
+import {useDispatch, useSelector} from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import {logoutTC} from '../state/login-reducer';
+import {getMeTC} from '../state/profile-reducer';
+import {AppRootStateType} from '../state/store';
+import s from './Profile.module.css';
+import {useEffect} from 'react';
 
 export function Profile() {
-  const isLoginIn = useSelector<AppRootStateType, boolean>(
-    (state) => state.login.isLoggedIn
-  );
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getMeTC())
+    }, []);
 
-  const logoutHandler = () => {
-    dispatch(logoutTC());
-  };
-  const getMeHandler = () => {
-    dispatch(getMeTC());
-  };
+    const isLoginIn = useSelector<AppRootStateType, boolean>(
+        (state) => state.login.isLoggedIn
+    );
 
+    const logoutHandler = () => {
+        dispatch(logoutTC());
+    };
 
-
-  if (!isLoginIn) {
-    return <Redirect to={"/login"} />;
-  }
-  return (
-    <div className={s.profile}>
-      <p>
-         Profile
-         <button onClick={getMeHandler}>Get me</button>
-      </p>
-
-      <div></div>
-      <div>
-        <button onClick={logoutHandler}>Sing out</button>
-      </div>
-    </div>
-  );
+    if (!isLoginIn) {
+        return <Redirect to={'/login'}/>;
+    }
+    return (
+        <div className={s.profile}>
+            <div>
+                <button onClick={logoutHandler}>Sing out</button>
+            </div>
+        </div>
+    );
 }
