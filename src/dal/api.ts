e1ba4c217e431692a9add978832d5bb091a4b5e0
elizavetaspivak <elizazbethspivak99@gmail.com>
2021-08-13 16:34:05 +0300

    refactor api

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -149,7 +149,7 @@ export type UpdatedUserDataType = {
 
 
 const instance = axios.create({
-	baseURL: "http://localhost:7542/2.0/",
+	baseURL: "https://neko-back.herokuapp.com/2.0/",
 	withCredentials: true,
 })
 
